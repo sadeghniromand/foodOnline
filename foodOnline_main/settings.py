ed78c268d04326f0facab45b58808d1d2087bb00
sadeghniromand <msadegh.niromand@gmail.com>
2023-07-16 17:02:23 +0330

    create account app and custom user model and manager

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
